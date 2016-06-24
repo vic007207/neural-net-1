ccdc6f23b0ce1ad7c84a90f393370635922ebb61
vic007207 <vic3df@virginia.edu>
2016-06-24 11:54:22 -0400

    added dbn tutorial

---
+++ dbn.py
@@ -0,0 +1,9 @@
+#credit http://www.pyimagesearch.com/2014/09/22/getting-started-deep-learning-python/
+
+# import the necessary packages
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+from sklearn import datasets
+from nolearn.dbn import DBN
+import numpy as np
+import cv2
